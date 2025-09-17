8e691ab4f38fd7d17e0aa0567c50f4dda0fa65de
Chris Oje <chroje@microsoft.com>
2025-09-17 16:54:55 -0700

    new training complete

--- TrainArena/Assets/Editor/QuickSkybox.cs
+++ TrainArena/Assets/Editor/QuickSkybox.cs
@@ -12,20 +12,20 @@ public static class QuickSkybox
         mat.SetColor("_GroundColor", new Color(0.26f,0.29f,0.34f));
         mat.SetFloat("_AtmosphereThickness", 0.95f);
         RenderSettings.skybox = mat;
-        var sun = Object.FindObjectOfType<Light>();
+        var sun = Object.FindFirstObjectByType<Light>();
         if (sun && sun.type == LightType.Directional) RenderSettings.sun = sun;
         DynamicGI.UpdateEnvironment();
     }
-    
-    [MenuItem("Tools/ML Hack/Quick Skybox/Simple Gradient")]
+
+    /*[MenuItem("Tools/ML Hack/Quick Skybox/Simple Gradient")]
     static void ApplyGradient()
     {
         var mat = new Material(Shader.Find("Skybox/Gradient"));
         mat.SetColor("_Top", new Color(0.38f,0.58f,1f));   // sky
         mat.SetColor("_Bottom", new Color(0.26f,0.29f,0.34f));
         RenderSettings.skybox = mat;
-        var sun = Object.FindObjectOfType<Light>();
+        var sun = Object.FindFirstObjectByType<Light>();
         if (sun && sun.type == LightType.Directional) RenderSettings.sun = sun;
         DynamicGI.UpdateEnvironment();
-    }
+    }*/
 }
